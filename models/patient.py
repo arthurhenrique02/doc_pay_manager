e77b30f98d543c487bc899ff7777c657ebb8d986
arthurhenrique02 <profissionalarthurhenrique@gmail.com>
2025-04-14 22:52:58 -0300

    fix: procedure relationship

--- models/patient.py
+++ models/patient.py
@@ -1,4 +1,5 @@
 from sqlalchemy import Column, Integer, String
+from sqlalchemy.orm import relationship
 
 from .base import Base
 
@@ -8,3 +9,5 @@ class Patient(Base):
 
     id = Column(Integer, primary_key=True, index=True)
     name = Column(String, nullable=False)
+
+    procedures = relationship("Procedure", back_populates="patient")
