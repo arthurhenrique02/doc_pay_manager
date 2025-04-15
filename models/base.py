307d1e745b7f5234596630ce2f8ba747a882d622
arthurhenrique02 <profissionalarthurhenrique@gmail.com>
2025-04-15 00:35:24 -0300

    feat: get and filter method. Filtering by range

--- models/base.py
+++ models/base.py
@@ -1,3 +1,4 @@
+from sqlalchemy import and_
 from sqlalchemy.orm import DeclarativeBase
 
 from .engine import get_db
@@ -31,6 +32,49 @@ class Base(DeclarativeBase):
             return True
         return False
 
+    def get(self, id: int):
+        return self.__database.query(self.__class__).get(id)
+
+    @classmethod
+    def filter(cls, **kwargs):
+        # pass kwargs by reference
+        range_filters = cls.__create_range_filter(kwargs)
+        remaining_filters = [
+            getattr(cls, key) == value for key, value in kwargs.items()
+        ]
+
+        print(remaining_filters)
+        all_filters = range_filters + remaining_filters
+
+        return (
+            cls.__database.query(cls).filter(and_(*all_filters)).order_by(cls.id.asc())
+        )
+
     def save(self):
         self.__database.commit()
         self.__database.refresh(self)
+
+    @classmethod
+    def __create_range_filter(cls, kwargs: dict) -> list:
+        range_filters = []
+
+        for key in list(kwargs.keys()):
+            # check if is a range filter. Get the columns and create the filter
+            if key.endswith("__range"):
+                col_name = key[: -len("__range")]
+                column = getattr(cls, col_name, None)
+                start, end = kwargs.pop(key)
+
+                # if column does not exist, raise an error
+                if column is None:
+                    raise ValueError(
+                        f"Column {col_name} does not exist in {cls.__class__}"
+                    )
+
+                if not start or not end:
+                    raise ValueError(
+                        "Start and end values are required for range filter"
+                    )
+
+                range_filters.append(column.between(start, end))
+        return range_filters
