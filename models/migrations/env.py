e3eb7ecd2a7cbc668967cfc63e2b53ce1c6d645f
arthurhenrique02 <profissionalarthurhenrique@gmail.com>
2025-04-15 21:43:17 -0300

    chore: mapped user model. Replaced doctor basemodel classes

--- models/migrations/env.py
+++ models/migrations/env.py
@@ -5,9 +5,10 @@ from alembic import context
 from sqlalchemy import engine_from_config, pool
 
 from models.base import Base
-from models.doctor import Doctor
-from models.patient import Patient
-from models.procedure import Procedure
+from models.doctor import Doctor  # noqa: F401
+from models.patient import Patient  # noqa: F401
+from models.procedure import Procedure  # noqa: F401
+from models.user import User  # noqa: F401
 
 # this is the Alembic Config object, which provides
 # access to the values within the .ini file in use.
--- routers/doctor.py
+++ routers/doctor.py
@@ -4,10 +4,9 @@ And used to test the Procedure API.
 """
 
 from fastapi import APIRouter
-from pydantic import BaseModel, ConfigDict
 
 # from fastapi.responses import JSONResponse
-from models.doctor import Doctor
+from models.doctor import Doctor, DoctorDetail, NewDoctor
 
 blueprint_name = "doctor"
 
@@ -16,16 +15,6 @@ router = APIRouter(
     tags=[blueprint_name],
     responses={404: {"description": "Not found"}},
 )
-
-
-class NewDoctor(BaseModel):
-    name: str
-
-
-class DoctorDetail(NewDoctor):
-    model_config = ConfigDict(from_attributes=True)
-
-    id: int
 
 
 @router.post("/registry", response_model=DoctorDetail)
